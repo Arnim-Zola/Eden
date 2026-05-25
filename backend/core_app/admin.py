767c306e02797ec411d60991df62bdceff115a81
Madhavaks7 <madhavapuranik46648@gmail.com>
2026-05-25 13:00:00 +0530

    feat: Register AnalysisReport model in Django admin

--- backend/core_app/admin.py
+++ backend/core_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AnalysisReport
 
-# Register your models here.
+@admin.register(AnalysisReport)
+class AnalysisReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'updated_at')
+    search_fields = ('id',)
