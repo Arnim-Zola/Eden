392f8a1552a8833171e893362a47d3d3be41eaac
Madhavaks7 <madhavapuranik46648@gmail.com>
2026-05-27 17:00:00 +0530

    feat: Register ClaimRecord model in Django admin

--- backend/core_app/admin.py
+++ backend/core_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AnalysisReport, AnalysisJob, MediaAsset
+from .models import AnalysisReport, AnalysisJob, MediaAsset, ClaimRecord
 
 @admin.register(AnalysisReport)
 class AnalysisReportAdmin(admin.ModelAdmin):
@@ -17,3 +17,10 @@ class MediaAssetAdmin(admin.ModelAdmin):
     list_display = ('id', 'job', 'asset_type', 'file_path', 'file_size', 'processing_status')
     list_filter = ('asset_type', 'processing_status')
     search_fields = ('id', 'file_path')
+
+@admin.register(ClaimRecord)
+class ClaimRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'job', 'claim_text', 'detection_source', 'classification_label', 'confidence_score')
+    list_filter = ('detection_source', 'classification_label')
+    search_fields = ('claim_text', 'contextual_reasoning')
+
